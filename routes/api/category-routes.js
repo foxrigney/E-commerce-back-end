27404a599b2c39a0b1bf9d1dce74494443a00699
Fox Rigney <foxrigney@gmail.com>
2021-12-02 20:51:41 -0600

    restful routes as exemplified in activity 24

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,50 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get, ('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+
+  try {
+    const categoryData = await categoryData.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    }); if (!categoryData){
+      res.status(404).json({message: 'No category data found in this id!'})
+    };
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  const post
+
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+
 });
 
 module.exports = router;
