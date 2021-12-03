c18738d508fdf8fb1ea751823e752c33dc64abe4
Fox Rigney <foxrigney@gmail.com>
2021-12-02 21:07:37 -0600

    post and destroy updated

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -23,9 +23,10 @@ router.get('/:id', (req, res) => {
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
-    }); if (!categoryData){
-      res.status(404).json({message: 'No category data found in this id!'})
+    }); if (!categoryData) {
+      res.status(404).json({ message: 'No category data found in this id!' })
     };
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -35,18 +36,49 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
-  const post
+  try {
+    const categoryData = await Category.create({
+      Category_id: req.body.reader_id,
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+
 
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update({
+      category_name: req.body.category_name,
+
+    }, {
+      category_id: req.body.reader_id,
+    });
+
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+
+
 
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
-
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
